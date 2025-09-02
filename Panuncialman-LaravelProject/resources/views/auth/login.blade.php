33e6fc3e0a1e3748ac02310b9190e5d16ff96caf
angeliligan <angelsiligan03@gmail.com>
2025-09-02 21:13:54 +0800

    Added new login  feature

--- Panuncialman-LaravelProject/resources/views/auth/login.blade.php
+++ Panuncialman-LaravelProject/resources/views/auth/login.blade.php
@@ -2,46 +2,41 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <!-- Login Card -->
+        
+        <!-- Login Form -->
+        <form method="POST" action="{{ route('login') }}" class="text-gray-700">
+            @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <!-- Email -->
+            <label for="email" class="text-xs font-bold after:content-['*']">Email</label>     
+            <input id="email" type="email" name="email" :value="old('email')" required autofocus 
+                class="w-full p-2 mb-2 mt-1 outline-none focus:ring-2 focus:ring-orange-500" />
+            <x-input-error :messages="$errors->get('email')" class="mt-1 text-xs text-red-500" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <!-- Password -->
+            <label for="password" class="text-xs font-bold after:content-['*']">Password</label>
+            <input id="password" type="password" name="password" required autocomplete="current-password"
+                class="w-full p-2 mb-2 mt-1 outline-none focus:ring-2 focus:ring-orange-500" />
+            <x-input-error :messages="$errors->get('password')" class="mt-1 text-xs text-red-500" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <!-- Remember Me -->
+            <label for="remember_me" class="inline-flex items-center mb-2">
+                <input id="remember_me" type="checkbox" name="remember" 
+                    class="rounded border-gray-300 text-orange-500 shadow-sm focus:ring-orange-500">
+                <span class="ms-2 text-xs text-gray-600">Remember me</span>
             </label>
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
+            <!-- Forgot Password -->
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                <a href="{{ route('password.request') }}" class="block text-right text-xs text-orange-500 mb-4">
+                    Forgot Password?
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+            <!-- Submit -->
+            <button type="submit" class="w-full rounded bg-orange-500 text-sky-50 p-2 text-center font-bold hover:bg-orange-400">
+                Log In
+            </button>
+        </form>
 </x-guest-layout>
