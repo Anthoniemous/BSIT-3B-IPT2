5bc86f3aa8b7b030a71b450bc737d380c1210a1a
Akazaki21 <kawaii.ako12@gmail.com>
2025-09-05 04:38:30 +0800

    Updated Panuncialman-LaravelProject

--- Panuncialman-LaravelProject/resources/views/layouts/app.blade.php
+++ Panuncialman-LaravelProject/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
+        <link rel="shortcut icon" href="{{asset('img/boombot_logo.png')}}" type="image/x-icon">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
